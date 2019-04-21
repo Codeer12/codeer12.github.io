5d8bcab5e9cd9919a66b5d3432a5722069306407
J TheCoder <maxgamertv12@gmail.com>
2019-04-21 13:03:06 -0400

    Remove web-start

--- web-start/scripts/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-	"apiKey": "AIzaSyA8r2vrQaL3f4Jz1Et09RPvJts2xR_sBOg",
-	"databaseURL": "https://login-to-games4u.firebaseio.com",
-	"storageBucket": "login-to-games4u.appspot.com",
-	"authDomain": "login-to-games4u.firebaseapp.com",
-	"messagingSenderId": "152743638512",
-	"projectId": "login-to-games4u"
-});
